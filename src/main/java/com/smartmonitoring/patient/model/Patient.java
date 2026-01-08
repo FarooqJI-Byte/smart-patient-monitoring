0c4accb68e9e6e526e043045dc6be0808b29cd4c
Farooq <faroo@Farooq04>
2026-01-08 21:51:51 +0530

    Day 1: Added Patient entity with JPA mapping

--- src/main/java/com/smartmonitoring/patient/model/Patient.java
+++ src/main/java/com/smartmonitoring/patient/model/Patient.java
@@ -1,5 +1,38 @@
 package com.smartmonitoring.patient.model;
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    private Integer age;
+
+    private String gender;
+
+    @Column(unique = true, nullable = false)
+    private String contactNumber;
+
+    private LocalDate admittedDate;
 
 }
