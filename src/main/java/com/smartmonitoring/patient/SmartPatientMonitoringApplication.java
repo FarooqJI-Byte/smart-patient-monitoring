564e527d504b31c4ca614a6ea498a9c01c1fa9ff
Farooq <faroo@Farooq04>
2026-01-08 21:37:00 +0530

    Day 1: Project setup and Git configuration

---
+++ src/main/java/com/smartmonitoring/patient/SmartPatientMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.smartmonitoring.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPatientMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPatientMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartmonitoring/patient/SmartPatientMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmonitoring.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPatientMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
